483000bdd5ff8068a51958412d0529d4f0ec12ce
Tuan"Thomas" Tran Quang <tranquangtuan060703@gmail.com>
2024-01-04 14:46:26 -0400

    14:45, 01/04/2024: Finished the back-end.

--- NodeJS/controllers/employeeController.js
+++ NodeJS/controllers/employeeController.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 var router = express.Router();
 var { Employee } = require('../models/employee.js');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 // router.get('/', (req, res) => {
 //     Employee.find((err, docs) => {
@@ -18,6 +19,32 @@ router.get('/', async (req, res) => {
         res.send(docs);
     } catch (err) {
         console.log('Error occurred in Retrieving Employees: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+// router.get('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id)) {
+//         return res.status(400).send(`No record with given id: ${req.params.id}`);
+//     }
+
+//     Employee.findById(req.params.id, (err, doc) => {
+//         if (!err) {
+//             res.send(doc);
+//         } else {
+//             console.log('Error occurred in Retrieving Employee: ' + JSON.stringify(err, undefined, 2));
+//         }
+//     });
+// });
+router.get('/:id', async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send(`No record with given id: ${req.params.id}`);
+    }
+
+    try {
+        const doc = await Employee.findById(req.params.id);
+        res.send(doc);
+    } catch (err) {
+        console.log('Error occurred in Retrieving Employee: ' + JSON.stringify(err, undefined, 2));
     }
 });
 
@@ -48,6 +75,72 @@ router.post('/', async (req, res) => {
         res.send(doc);
     } catch (err) {
         console.log('Error occurred in Employee Save: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+// router.put('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id)) {
+//         return res.status(400).send(`No record with given id: ${req.params.id}`);
+//     }
+
+//     var emp = {
+//         name: req.body.name,
+//         position: req.body.position,
+//         office: req.body.office,
+//         salary: req.body.salary
+//     };
+
+//     Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true }, (err, doc) => {
+//         if (!err) {
+//             res.send(doc);
+//         } else {
+//             console.log('Error occurred in Employee Update: ' + JSON.stringify(err, undefined, 2));
+//         }
+//     });
+// });
+router.put('/:id', async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send(`No record with given id: ${req.params.id}`);
+    }
+
+    var emp = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+
+    try {
+        const doc = await Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true });
+        res.send(doc);
+    } catch (err) {
+        console.log('Error occurred in Employee Update: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+// router.delete('/:id', (req, res) => {
+//     if (!ObjectId.isValid(req.params.id)) {
+//         return res.status(400).send(`No record with given id: ${req.params.id}`);
+//     }
+
+//     Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+//         if (!err) {
+//             res.send(doc);
+//         } else {
+//             console.log('Error occurred in Employee Delete: ' + JSON.stringify(err, undefined, 2));
+//         }
+//     });
+// });
+router.delete('/:id', async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send(`No record with given id: ${req.params.id}`);
+    }
+
+    try {
+        const doc = await Employee.findByIdAndRemove(req.params.id);
+        res.send(doc);
+    } catch (err) {
+        console.log('Error occurred in Employee Delete: ' + JSON.stringify(err, undefined, 2));
     }
 });
 
