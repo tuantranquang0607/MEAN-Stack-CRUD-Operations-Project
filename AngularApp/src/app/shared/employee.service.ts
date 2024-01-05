e814e8de9cde65136fc546a17322056a06a7fe82
Tuan"Thomas" Tran Quang <tranquangtuan060703@gmail.com>
2024-01-05 14:58:06 -0400

    14:55, 01/05/2023: I finished following the tutorial and ended up with tons of bugs at the front-end. I guessed it was because of the old tutorial, so I would spend some time fixing it.

--- AngularApp/src/app/shared/employee.service.ts
+++ AngularApp/src/app/shared/employee.service.ts
@@ -12,6 +12,23 @@ import 'rxjs/add/operator/toPromise';
 export class EmployeeService {
   selectedEmployee: Employee;
   employees: Employee[];
+  readonly baseURL = 'http://localhost:3000/employees';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postEmployee(emp: Employee) {
+    return this.http.post(this.baseURL, emp);
+  }
+
+  getEmployeeList() {
+    return this.http.get(this.baseURL);
+  }
+
+  putEmployee(emp: Employee) {
+    return this.http.put(this.baseURL + `/${emp._id}`, emp);
+  }
+
+  deleteEmployee(_id: string) {
+    return this.http.delete(this.baseURL + `/${_id}`);
+  }
 }
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { mongoose } = require('./db.js');
+const cors = require('cors');
 
 var employeeController = require('./controllers/employeeController.js');
 var app = express();
@@ -8,3 +9,4 @@ var app = express();
 app.use(bodyParser.json());
 app.use('/employees', employeeController);
 app.listen(3000, () => console.log('Server started at port: http://localhost:3000/employees/'));
+app.use(cors({ origin: 'http://localhost:4200' }));
